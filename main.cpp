4d1eda51020a3782182a97ab7c338eab1531af41
swagatbora90 <swagatbora90@gmail.com>
2013-09-26 09:07:50 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+cout<<"\nThis is a program to print the fibonacci sequence";
+cout<<"\n The printing stops when the user inputs any key";
+
+long int firstNo = 1;
+long int secNo =1;
+long int sum=0;
+
+cout<<endl<<firstNo;
+sleep(1000);
+cout<<endl<<secNo;
+sleep(1000);
+while(!getchar())
+  {
+  sum=firstNo+secNo;
+  cout<<endl<<sum;
+  firstNo=secNo;
+  secNo=sum;
+  sleep(1000);
+  
+  }
+
+cout<<"\n Thank you for your time..Have a good day";
+}
